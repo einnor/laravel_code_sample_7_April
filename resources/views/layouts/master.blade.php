fe01deb1fda6e6f375ecf488ea45517d4aad9392
Ronnie Nyaga <ronnienyaga@gmail.com>
2016-04-08 15:56:11 +0300

    Added styling and links to home page

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -15,6 +15,36 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <style>
+      html, body {
+        height: 100%;
+      }
+
+      body {
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        display: table;
+        font-weight: 100;
+        /*font-family: 'Lato';*/
+      }
+
+      .container {
+        /*text-align: center;*/
+        display: table-cell;
+        /*vertical-align: middle;*/
+      }
+
+      .content {
+        text-align: center;
+        display: inline-block;
+        vertical-align: middle;
+      }
+
+      .title {
+        font-size: 48px;
+      }
+    </style>
   </head>
   <body>
     <div class="container">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,8 @@
     <head>
         <title>Laravel</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        {{--<link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">--}}
+        <link rel="stylesheet" href="/css/app.css" >
 
         <style>
             html, body {
@@ -16,35 +17,33 @@
                 width: 100%;
                 display: table;
                 font-weight: 100;
-                font-family: 'Lato';
+                /*font-family: 'Lato';*/
             }
 
             .container {
-                text-align: center;
+                /*text-align: center;*/
                 display: table-cell;
-                vertical-align: middle;
+                /*vertical-align: middle;*/
             }
 
             .content {
-                text-align: center;
-                display: inline-block;
-            }
+                 text-align: center;
+                 display: inline-block;
+                vertical-align: middle;
+             }
 
             .title {
-                font-size: 96px;
+                font-size: 48px;
             }
         </style>
     </head>
     <body>
         <div class="container">
+            @include('partials.nav')
             <div class="content">
-                <div class="title">Laravel 5</div>
-                <p>
-                  Welcome, Ronnie. You made it.
-                </p>
-                <p>
-                  But don't get too comfortable; we're just getting started!   :-)
-                </p>
+
+
+
             </div>
         </div>
     </body>
